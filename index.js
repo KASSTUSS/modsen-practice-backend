118f603af51a4f8fe2ea79e1c0d4278521981800
KASSTUSS <kostya2003112015@gmail.com>
2023-06-25 19:05:34 +0300

    fix: fix work

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const fetching = async (url) => {
 
 app.get("/volumes", (req, res) => {
   const requestToBooksApi =
-    "https://www.googleapis.com/books/v1/volumes?" +
-    decodeURI(req.url).split("?")[1];
+    "https://www.googleapis.com/books/v1/volumes" +
+    decodeURI(req.url).split("/volumes")[1];
   fetching(requestToBooksApi).then((responceFromBooksApi) => {
     res.json({
       responceFromBooksApi,
