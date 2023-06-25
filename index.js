d572f667dc14be2cd12d458575659ed765ddd769
KASSTUSS <kostya2003112015@gmail.com>
2023-06-25 18:23:06 +0300

    fix: add PORT to env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 
 const fetching = async (url) => {
   const { data } = await axios.get(url, {});
